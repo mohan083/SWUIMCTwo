b8b5f4714973454b0f29e22cbf9987656398c219
Mohan Chaudhari <mohan.chaudhari@globant.com>
2022-07-29 23:20:57 +0530

    Initial Commit

---
+++ SWUIMCTwo/SWUIMCTwo/SWUIMCTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWUIMCTwoApp.swift
+//  SWUIMCTwo
+//
+//  Created by Mohan Chaudhari on 29/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SWUIMCTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
